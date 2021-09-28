a35b5bd6a550d2e206492f7a7c3d907cf2ad01a9
Chloé Debyser <chloe.debyser@gmail.com>
2021-09-28 17:43:40 -0400

    Commented out the rest of the code

--- DBtoRegistry_Pilot.R
+++ DBtoRegistry_Pilot.R
@@ -105,113 +105,112 @@ arc.write(paste0(outputDB, "/KBA_Website"), KBA_Website, overwrite = T)
 
 
 
-
-# PICK UP HERE
-# System
-# Create
-System <- data.frame(Type_EN = unlist(strsplit(KBASite$Systems, "; "))) %>%
-  mutate(Type_FR = NA) %>%
-  mutate(SystemID = 1:nrow(.)) %>%
-  relocate(SystemID, before=Type_EN)
-
-# Save
-arc.write("C:/Users/CDebyser/OneDrive - Wildlife Conservation Society/4. Analyses/5. Pipeline with Birds Canada/BirdsCanada_TrialIslands.gdb/System", System)
-
-# Habitat
-# Create
-Habitat <- data.frame(Habitat_EN = KBAHabitat$Habitat) %>%
-  mutate(Habitat_FR = NA,
-         Description = NA,
-         HabitatCode = NA,
-         HabitatID = 1:nrow(.)) %>%
-  mutate(SystemID = sapply(Habitat_EN, function(x) ifelse(x %in% c("Grassland", "Shrubland", "Savanna"),
-                                                          System$SystemID[which(System$Type_EN == "Terrestrial")],
-                                                          NA))) %>%
-  relocate(HabitatID, SystemID, HabitatCode, before=Habitat_EN)
-
-# Save
-arc.write("C:/Users/CDebyser/OneDrive - Wildlife Conservation Society/4. Analyses/5. Pipeline with Birds Canada/BirdsCanada_TrialIslands.gdb/Habitat", Habitat)
-
-# KBA_Habitat
-# Create
-KBA_Habitat <- KBAHabitat %>%
-  mutate(HabitatSiteID = 1:nrow(.)) %>%
-  mutate(SiteID = siteCode) %>%
-  rename(Habitat_EN = Habitat) %>%
-  mutate(HabitatID = sapply(Habitat_EN, function(x) Habitat$HabitatID[which(Habitat$Habitat_EN == x)])) %>%
-  mutate(PercentCoverMin = sapply(PercentCover, function(x) substr(x, 1, stri_locate_all(pattern = '-', x, fixed = TRUE)[[1]][1,1]-1))) %>%
-  mutate(PercentCoverMin = as.integer(PercentCoverMin)) %>%
-  mutate(PercentCoverMax = sapply(PercentCover, function(x) substr(x, stri_locate_all(pattern = '-', x, fixed = TRUE)[[1]][1,1]+1, nchar(x)-1))) %>%
-  mutate(PercentCoverMax = as.integer(PercentCoverMax)) %>%
-  select(HabitatSiteID, SiteID, HabitatID, PercentCoverMin, PercentCoverMax)
-
-# Save
-arc.write("C:/Users/CDebyser/OneDrive - Wildlife Conservation Society/4. Analyses/5. Pipeline with Birds Canada/BirdsCanada_TrialIslands.gdb/KBA_Habitat", KBA_Habitat)
-
-# Threats
-# Create
-Threats <- KBAThreat %>%
-  mutate(LastLevel = sapply(1:nrow(KBAThreat), function(x) sum(!is.na(KBAThreat[x, c("Level1", "Level2", "Level3")])))) %>%
-  mutate(Threat_EN = ifelse(LastLevel == 1,
-                            Level1,
-                            ifelse(LastLevel == 2,
-                                   Level2,
-                                   Level3))) %>%
-  select(Threat_EN) %>%
-  distinct() %>%
-  mutate(Threat_FR = NA,
-         ThreatCode = sapply(Threat_EN, function(x) substr(x, 1, stri_locate_all(pattern=" ", x, fixed=T)[[1]][1,1]-1)),
-         Threat_EN = sapply(Threat_EN, function(x) substr(x, stri_locate_all(pattern=" ", x, fixed=T)[[1]][1,1]+1, nchar(x))),
-         ThreatID = 1:nrow(.)) %>%
-  select(ThreatID, ThreatCode, Threat_EN, Threat_FR)
-
-# Save
-arc.write("C:/Users/CDebyser/OneDrive - Wildlife Conservation Society/4. Analyses/5. Pipeline with Birds Canada/BirdsCanada_TrialIslands.gdb/Threats", Threats)
-
-# KBA_Threats
-# Create
-KBA_Threats <- KBAThreat %>%
-  mutate(LastLevel = sapply(1:nrow(KBAThreat), function(x) sum(!is.na(KBAThreat[x, c("Level1", "Level2", "Level3")])))) %>%
-  mutate(Threat_EN = ifelse(LastLevel == 1,
-                            Level1,
-                            ifelse(LastLevel == 2,
-                                   Level2,
-                                   Level3))) %>%
-  mutate(ThreatCode = sapply(Threat_EN, function(x) substr(x, 1, stri_locate_all(pattern=" ", x, fixed=T)[[1]][1,1]-1))) %>%
-  select(ThreatCode, Timing, Scope, Severity) %>%
-  distinct() %>%
-  mutate(ThreatID = Threats$ThreatID[which(Threats$ThreatCode == ThreatCode)],
-         SiteID = siteCode,
-         ThreatsSiteID = 1:nrow(.)) %>%
-  select(ThreatsSiteID, SiteID, ThreatID, Timing, Scope, Severity)
-
-# Save
-arc.write("C:/Users/CDebyser/OneDrive - Wildlife Conservation Society/4. Analyses/5. Pipeline with Birds Canada/BirdsCanada_TrialIslands.gdb/KBA_Threats", KBA_Threats)
-
-# Conservation
-# Create
-Conservation <- KBAAction %>%
-  select(ConservationAction) %>%
-  distinct() %>%
-  mutate(ConservationCode = sapply(ConservationAction, function(x) substr(x, 1, stri_locate_all(pattern = ' ', x, fixed = TRUE)[[1]][1,1]-1)),
-         ConservationAction_EN = sapply(ConservationAction, function(x) substr(x, stri_locate_all(pattern = ' ', x, fixed = TRUE)[[1]][1,1]+1, nchar(x))),
-         ConservationAction_FR = NA,
-         ConservationID = 1:nrow(.)) %>%
-  select(ConservationID, ConservationCode, ConservationAction_EN, ConservationAction_FR)
-
-# Save
-arc.write("C:/Users/CDebyser/OneDrive - Wildlife Conservation Society/4. Analyses/5. Pipeline with Birds Canada/BirdsCanada_TrialIslands.gdb/Conservation", Conservation)
-
-# KBA_Conservation
-# Create
-KBA_Conservation <- KBAAction %>%
-  select(OngoingOrNeeded, ConservationAction) %>%
-  mutate(ConservationID = Conservation$ConservationID[which(paste(Conservation$ConservationCode, Conservation$ConservationAction_EN, sep=" ") == ConservationAction)],
-         SiteID = siteCode,
-         ConservationSiteID = 1:nrow(.)) %>%
-  rename(Ongoing_Needed = OngoingOrNeeded) %>%
-  select(ConservationSiteID, SiteID, ConservationID, Ongoing_Needed)
-
-# Save
-arc.write("C:/Users/CDebyser/OneDrive - Wildlife Conservation Society/4. Analyses/5. Pipeline with Birds Canada/BirdsCanada_TrialIslands.gdb/KBA_Conservation", KBA_Conservation)
-
+# # PICK UP HERE
+# # System
+# # Create
+# System <- data.frame(Type_EN = unlist(strsplit(KBASite$Systems, "; "))) %>%
+#   mutate(Type_FR = NA) %>%
+#   mutate(SystemID = 1:nrow(.)) %>%
+#   relocate(SystemID, before=Type_EN)
+# 
+# # Save
+# arc.write("C:/Users/CDebyser/OneDrive - Wildlife Conservation Society/4. Analyses/5. Pipeline with Birds Canada/BirdsCanada_TrialIslands.gdb/System", System)
+# 
+# # Habitat
+# # Create
+# Habitat <- data.frame(Habitat_EN = KBAHabitat$Habitat) %>%
+#   mutate(Habitat_FR = NA,
+#          Description = NA,
+#          HabitatCode = NA,
+#          HabitatID = 1:nrow(.)) %>%
+#   mutate(SystemID = sapply(Habitat_EN, function(x) ifelse(x %in% c("Grassland", "Shrubland", "Savanna"),
+#                                                           System$SystemID[which(System$Type_EN == "Terrestrial")],
+#                                                           NA))) %>%
+#   relocate(HabitatID, SystemID, HabitatCode, before=Habitat_EN)
+# 
+# # Save
+# arc.write("C:/Users/CDebyser/OneDrive - Wildlife Conservation Society/4. Analyses/5. Pipeline with Birds Canada/BirdsCanada_TrialIslands.gdb/Habitat", Habitat)
+# 
+# # KBA_Habitat
+# # Create
+# KBA_Habitat <- KBAHabitat %>%
+#   mutate(HabitatSiteID = 1:nrow(.)) %>%
+#   mutate(SiteID = siteCode) %>%
+#   rename(Habitat_EN = Habitat) %>%
+#   mutate(HabitatID = sapply(Habitat_EN, function(x) Habitat$HabitatID[which(Habitat$Habitat_EN == x)])) %>%
+#   mutate(PercentCoverMin = sapply(PercentCover, function(x) substr(x, 1, stri_locate_all(pattern = '-', x, fixed = TRUE)[[1]][1,1]-1))) %>%
+#   mutate(PercentCoverMin = as.integer(PercentCoverMin)) %>%
+#   mutate(PercentCoverMax = sapply(PercentCover, function(x) substr(x, stri_locate_all(pattern = '-', x, fixed = TRUE)[[1]][1,1]+1, nchar(x)-1))) %>%
+#   mutate(PercentCoverMax = as.integer(PercentCoverMax)) %>%
+#   select(HabitatSiteID, SiteID, HabitatID, PercentCoverMin, PercentCoverMax)
+# 
+# # Save
+# arc.write("C:/Users/CDebyser/OneDrive - Wildlife Conservation Society/4. Analyses/5. Pipeline with Birds Canada/BirdsCanada_TrialIslands.gdb/KBA_Habitat", KBA_Habitat)
+# 
+# # Threats
+# # Create
+# Threats <- KBAThreat %>%
+#   mutate(LastLevel = sapply(1:nrow(KBAThreat), function(x) sum(!is.na(KBAThreat[x, c("Level1", "Level2", "Level3")])))) %>%
+#   mutate(Threat_EN = ifelse(LastLevel == 1,
+#                             Level1,
+#                             ifelse(LastLevel == 2,
+#                                    Level2,
+#                                    Level3))) %>%
+#   select(Threat_EN) %>%
+#   distinct() %>%
+#   mutate(Threat_FR = NA,
+#          ThreatCode = sapply(Threat_EN, function(x) substr(x, 1, stri_locate_all(pattern=" ", x, fixed=T)[[1]][1,1]-1)),
+#          Threat_EN = sapply(Threat_EN, function(x) substr(x, stri_locate_all(pattern=" ", x, fixed=T)[[1]][1,1]+1, nchar(x))),
+#          ThreatID = 1:nrow(.)) %>%
+#   select(ThreatID, ThreatCode, Threat_EN, Threat_FR)
+# 
+# # Save
+# arc.write("C:/Users/CDebyser/OneDrive - Wildlife Conservation Society/4. Analyses/5. Pipeline with Birds Canada/BirdsCanada_TrialIslands.gdb/Threats", Threats)
+# 
+# # KBA_Threats
+# # Create
+# KBA_Threats <- KBAThreat %>%
+#   mutate(LastLevel = sapply(1:nrow(KBAThreat), function(x) sum(!is.na(KBAThreat[x, c("Level1", "Level2", "Level3")])))) %>%
+#   mutate(Threat_EN = ifelse(LastLevel == 1,
+#                             Level1,
+#                             ifelse(LastLevel == 2,
+#                                    Level2,
+#                                    Level3))) %>%
+#   mutate(ThreatCode = sapply(Threat_EN, function(x) substr(x, 1, stri_locate_all(pattern=" ", x, fixed=T)[[1]][1,1]-1))) %>%
+#   select(ThreatCode, Timing, Scope, Severity) %>%
+#   distinct() %>%
+#   mutate(ThreatID = Threats$ThreatID[which(Threats$ThreatCode == ThreatCode)],
+#          SiteID = siteCode,
+#          ThreatsSiteID = 1:nrow(.)) %>%
+#   select(ThreatsSiteID, SiteID, ThreatID, Timing, Scope, Severity)
+# 
+# # Save
+# arc.write("C:/Users/CDebyser/OneDrive - Wildlife Conservation Society/4. Analyses/5. Pipeline with Birds Canada/BirdsCanada_TrialIslands.gdb/KBA_Threats", KBA_Threats)
+# 
+# # Conservation
+# # Create
+# Conservation <- KBAAction %>%
+#   select(ConservationAction) %>%
+#   distinct() %>%
+#   mutate(ConservationCode = sapply(ConservationAction, function(x) substr(x, 1, stri_locate_all(pattern = ' ', x, fixed = TRUE)[[1]][1,1]-1)),
+#          ConservationAction_EN = sapply(ConservationAction, function(x) substr(x, stri_locate_all(pattern = ' ', x, fixed = TRUE)[[1]][1,1]+1, nchar(x))),
+#          ConservationAction_FR = NA,
+#          ConservationID = 1:nrow(.)) %>%
+#   select(ConservationID, ConservationCode, ConservationAction_EN, ConservationAction_FR)
+# 
+# # Save
+# arc.write("C:/Users/CDebyser/OneDrive - Wildlife Conservation Society/4. Analyses/5. Pipeline with Birds Canada/BirdsCanada_TrialIslands.gdb/Conservation", Conservation)
+# 
+# # KBA_Conservation
+# # Create
+# KBA_Conservation <- KBAAction %>%
+#   select(OngoingOrNeeded, ConservationAction) %>%
+#   mutate(ConservationID = Conservation$ConservationID[which(paste(Conservation$ConservationCode, Conservation$ConservationAction_EN, sep=" ") == ConservationAction)],
+#          SiteID = siteCode,
+#          ConservationSiteID = 1:nrow(.)) %>%
+#   rename(Ongoing_Needed = OngoingOrNeeded) %>%
+#   select(ConservationSiteID, SiteID, ConservationID, Ongoing_Needed)
+# 
+# # Save
+# arc.write("C:/Users/CDebyser/OneDrive - Wildlife Conservation Society/4. Analyses/5. Pipeline with Birds Canada/BirdsCanada_TrialIslands.gdb/KBA_Conservation", KBA_Conservation)
+# 
