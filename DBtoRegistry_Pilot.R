ecbd93fc649926462005a60de4fbf954ba0590cb
Chloé Debyser <chloe.debyser@gmail.com>
2022-02-11 19:23:01 -0500

    Add IUCNStatus, COSEWICStatus, Species, and Species_Citation

--- DBtoRegistry_Pilot.R
+++ DBtoRegistry_Pilot.R
@@ -4,7 +4,12 @@
 
 #### KBA-EBAR to KBA Registry - Trial Islands & Yukon Pilot
 #### Needed: Way to assign unique IDs (e.g. ThreatsSiteID, ConservationSiteID)
+#### Needed: Way to match new records to previously existing IDs (e.g. for citations - if a given citation was already used in a previous KBA proposal)
 #### Needed: Don't send global sites to Registry if they don't have a WDKBAID
+#### Needed: Add considerations of species sensitivity
+#### Needed: Filter internal boundaries
+#### Needed: Find substitutes for everything that is hard coded
+#### Needed: Add SARA_STATUS_DATE when available in BIOTICS_ELEMENT_NATIONAL
 
 #### Workspace ####
 # Packages
@@ -15,6 +20,9 @@ library(sf)
 library(arcgisbinding)
 arc.check_product()
 library(stringi)
+
+# Portal URL
+url <- "https://gis.natureserve.ca/arcgis/rest/services/"
 
 # Input parameters
 inputDB <- "C:/Users/CDebyser/OneDrive - Wildlife Conservation Society/4. Analyses/5. Pipeline - KBA-EBAR to Registry/KBA-EBAR_Pilot.gdb"
@@ -50,30 +58,91 @@ KBAHabitat <- arc.open(paste0(inputDB, "/KBAHabitat")) %>%
 KBACitation <- arc.open(paste0(inputDB, "/KBACitation")) %>%
   arc.select()
 
+Species <- arc.open(paste0(inputDB, "/Species")) %>%
+  arc.select()
+
+SpeciesAtSite <- arc.open(paste0(inputDB, "/SpeciesAtSite")) %>%
+  arc.select()
+
+SpeciesAssessment <- arc.open(paste0(inputDB, "/SpeciesAssessment")) %>%
+  arc.select()
+
+PopSizeCitation <- arc.open(paste0(inputDB, "/PopSizeCitation")) %>%
+  arc.select()
+
+KBAInputPolygon <- arc.open(paste0(inputDB, "/KBAInputPolygon")) %>%
+  arc.select()
+
+KBACustomPolygon <- arc.open(paste0(inputDB, "/KBACustomPolygon")) %>%
+  arc.select() %>%
+  arc.data2sf()
+
       # Lookup tables
-KBA_Level <- read.csv("G:/My Drive/KBA Canada Team/1. Source Datasets/Birds Canada/LookUps_2021.09.21/KBA_Level.csv", sep=",", fileEncoding = "UTF-8-BOM")
-KBA_Province <- read.csv("G:/My Drive/KBA Canada Team/1. Source Datasets/Birds Canada/LookUps_2021.09.21/KBA_Province.csv", sep=",", fileEncoding = "UTF-8-BOM")
+KBA_Level <- read.csv("G:/My Drive/KBA Canada Team/1. Source Datasets/Birds Canada/LookUps_2022.02.11/KBA_Level.csv", sep=",", fileEncoding = "UTF-8-BOM")
+KBA_Province <- read.csv("G:/My Drive/KBA Canada Team/1. Source Datasets/Birds Canada/LookUps_2022.02.11/KBA_Province.csv", sep=",")
 Threats <- read.csv("G:/My Drive/KBA Canada Team/1. Source Datasets/Birds Canada/LookUps_2021.09.21/Threats.csv", sep="\t", fileEncoding = "UTF-8-BOM")
 Conservation <- read.csv("G:/My Drive/KBA Canada Team/1. Source Datasets/Birds Canada/LookUps_2021.09.21/Conservation.csv", sep=",", fileEncoding = "UTF-8-BOM")
 System <- read.csv("G:/My Drive/KBA Canada Team/1. Source Datasets/Birds Canada/LookUps_2021.09.21/System.csv", sep=",", fileEncoding = "UTF-8-BOM")
 Habitat <- read.xlsx("G:/My Drive/KBA Canada Team/1. Source Datasets/Birds Canada/LookUps_2021.09.21/Habitat.xlsx")
+AssessmentParameter <- read.csv("G:/My Drive/KBA Canada Team/1. Source Datasets/Birds Canada/LookUps_2022.02.11/AssessmentParameter.csv")
+COSEWICStatus <- read.csv("G:/My Drive/KBA Canada Team/1. Source Datasets/Birds Canada/LookUps_2022.02.11/COSEWICStatus.csv")
+Criterion <- read.csv("G:/My Drive/KBA Canada Team/1. Source Datasets/Birds Canada/LookUps_2022.02.11/Criterion.csv")
+IUCNStatus <- read.csv("G:/My Drive/KBA Canada Team/1. Source Datasets/Birds Canada/LookUps_2022.02.11/IUCNStatus.csv")
+Subcriterion <- read.csv("G:/My Drive/KBA Canada Team/1. Source Datasets/Birds Canada/LookUps_2022.02.11/Subcriterion.csv")
 
-#### Only retain information pertaining to ACCEPTED sites ####
+      # KBA-EBAR database
+biotics <- arc.open(paste0(url, 'EBAR-KBA/Restricted/FeatureServer/4')) %>%
+  arc.select()
+
+#### Only retain information relevant to the Registry ####
 # KBASite
-KBASite %<>% filter(SiteStatus == 6) # Only retain sites with SiteStatus = 6 (Accepted)
+KBASite %<>%
+  filter(Public == 1) # Only retain sites with Public = 1 (i.e. ready for public sharing)
 
 # KBAThreat
-KBAThreat %<>% filter(KBASiteID %in% KBASite$KBASiteID)
+KBAThreat %<>%
+  filter(KBASiteID %in% KBASite$KBASiteID)
 
 # KBAAction
-KBAAction %<>% filter(KBASiteID %in% KBASite$KBASiteID)
+KBAAction %<>%
+  filter(KBASiteID %in% KBASite$KBASiteID)
 
 # KBAHabitat
-KBAHabitat %<>% filter(KBASiteID %in% KBASite$KBASiteID)
+KBAHabitat %<>%
+  filter(KBASiteID %in% KBASite$KBASiteID)
 
 # KBACitation
-KBACitation %<>% filter(KBASiteID %in% KBASite$KBASiteID)
-  
+KBACitation %<>%
+  filter(KBASiteID %in% KBASite$KBASiteID)
+
+# SpeciesAtSite
+SpeciesAtSite %<>%
+  filter(KBASiteID %in% KBASite$KBASiteID) %>%
+  filter(MeetsCriteria == "Y") # Only retain species that meet KBA criteria
+
+# Species
+Species %<>%
+  filter(SpeciesID %in% SpeciesAtSite$SpeciesID)
+
+# SpeciesAssessment
+SpeciesAssessment %<>%
+  filter(SpeciesAtSiteID %in% SpeciesAtSite$SpeciesAtSiteID)
+
+# PopSizeCitation
+cols <- colnames(PopSizeCitation)
+PopSizeCitation %<>%
+  filter(SpeciesAssessmentID %in% SpeciesAssessment$SpeciesAssessmentID)
+
+# KBAInputPolygon
+KBAInputPolygon %<>%
+  filter(SpeciesAtSiteID %in% SpeciesAtSite$SpeciesAtSiteID) %>%
+  filter(SpatialScope == "S") # Only retain internal boundaries
+
+# KBACustomPolygon
+KBACustomPolygon %<>%
+  filter(SpeciesAtSiteID %in% SpeciesAtSite$SpeciesAtSiteID) %>%
+  filter(SpatialScope == "S") # Only retain internal boundaries
+
 #### KBA-EBAR database to KBA Registry ####
 # KBA_Level
 arc.write(paste0(outputDB, "/KBA_Level"), KBA_Level, overwrite = T)
@@ -88,12 +157,15 @@ KBA_Site <- KBASite %>%
          Assessed = StatusChangeDate,
          AltMin = AltitudeMin,
          AltMax = AltitudeMax,
+         Latitude = lat_wgs84,
+         Longitude = long_wgs84,
+         Area = AreaKM2,
          geometry = geom) %>%
   mutate(SiteID = 1:nrow(.),
          Name_FR = ifelse(is.na(NationalName_FR), Name_EN, NationalName_FR),
-         Level_EN = ifelse(grepl("Global", KBALevel), "Global", "National"),
+         Level_EN = ifelse(grepl("Global", KBALevel_EN), "Global", "National"),
          PercentProtected = NA) %>%
-  left_join(., KBA_Province, by=c("Jurisdiction" = "Province_EN")) %>%
+  left_join(., KBA_Province, by=c("Jurisdiction_EN" = "Province_EN")) %>%
   left_join(., KBA_Level, by="Level_EN") %>%
   select(all_of(crosswalk %>% filter(Layer_BC == "KBA_Site") %>% pull(Name_BC)))
 
@@ -183,8 +255,11 @@ KBA_System <- KBASite %>%
   left_join(., st_drop_geometry(KBA_Site[,c("SiteID", "SiteCode")]), by="SiteCode") %>%
   separate_rows(., Systems, sep="; ") %>%
   left_join(., System[,c("SystemID", "Type_EN")], by=c("Systems" = "Type_EN")) %>%
-  mutate(SystemSiteID = 1:nrow(.)) %>%
-  select(all_of(crosswalk %>% filter(Layer_BC == "KBA_System") %>% pull(Name_BC)))
+  mutate(SystemSiteID = 1:nrow(.),
+         Rank = NA) %>%
+  select(all_of(crosswalk %>% filter(Layer_BC == "KBA_System") %>% pull(Name_BC))) %>%
+  group_by(SiteID) %>%
+  mutate(Rank = 1:n())
   
       # Save
 arc.write(paste0(outputDB, "/KBA_System"), KBA_System, overwrite = T)
@@ -207,3 +282,124 @@ KBA_Habitat <- KBAHabitat %>%
 
       # Save
 arc.write(paste0(outputDB, "/KBA_Habitat"), KBA_Habitat, overwrite = T)
+
+# IUCNStatus
+arc.write(paste0(outputDB, "/IUCNStatus"), IUCNStatus, overwrite = T)
+
+# COSEWICStatus
+arc.write(paste0(outputDB, "/COSEWICStatus"), COSEWICStatus, overwrite = T)
+
+# Species
+      # Add BIOTICS_ELEMENT_NATIONAL information
+Species %<>%
+  left_join(., biotics, by=c("SpeciesID" = "speciesid"))
+
+      # Change field names to Title Case
+colnames(Species) <- tools::toTitleCase(colnames(Species))
+
+      # General formatting
+Species %<>%  
+  rename(Order = Tax_order,
+         ScientificName = National_scientific_name,
+         InformalTaxonomicGroup = RegistryTaxGroup,
+         CommonName_EN = National_engl_name,
+         CommonName_FR = National_fr_name,
+         WDKBASpecRecID = WDKBAID,
+         IUCNTaxonID = IUCN_InternalTaxonID,
+         IUCNAssessmentID = IUCN_AssessmentID,
+         IUCNAssessmentDate = IUCN_AssessmentDate,
+         IUCNPopulationTrend = IUCN_PopulationTrend,
+         NSElementCode = Element_code,
+         NSGlobalUniqueID = Global_unique_identifier,
+         NSElementGlobalID = Element_global_id,
+         COSEWICAssessmentDate = Cosewic_date,
+         COSEWICStatusCriteria = Cosewic_assess_criteria,
+         SARStatus = Sara_status,
+         NSGRank = G_rank,
+         NSGRankReviewDate = G_rank_review_date,
+         NSNRank = N_rank,
+         NSNRankReviewDate = N_rank_review_date) %>%
+  mutate(Sensitive = 0,
+         Subspecies_EN = NA,
+         Subspecies_FR = NA,
+         Population_EN = NA,
+         Population_FR = NA,
+         BirdAlphaCode = NA,
+         BCSpeciesID = NA,
+         IUCNLink = NA,
+         SARAssessmentDate = NA,
+         COSEWICLink = NA,
+         NSLink = NA,
+         ContinentalPopulationSize = NA,
+         CitationContinentalPopulation = NA) %>%
+  left_join(., IUCNStatus, by=c("IUCN_CD" = "Nomenclature")) %>%
+  left_join(., COSEWICStatus, by=c("Cosewic_status" = "Nomenclature"))
+
+      # Range information
+            # Unnest current_distribution
+range <- Species %>%
+  select(Current_distribution, SpeciesID) %>%
+  mutate(Current_distribution = strsplit(Current_distribution, ", ")) %>%
+  unnest(Current_distribution) %>%
+  mutate(Current_distribution = ifelse(Current_distribution %in% c("LB", "NF"), "NL", Current_distribution)) %>%
+  left_join(., KBA_Province, by=c("Current_distribution" = "Abbreviation"))
+
+            # Range_EN
+range_en <- range %>%
+  select(SpeciesID, Province_EN) %>%
+  group_by(SpeciesID) %>%
+  arrange(SpeciesID, Province_EN) %>%
+  summarise(Range_EN = paste(Province_EN, collapse = ", "))
+
+            # Range_FR
+range_fr <- range %>%
+  select(SpeciesID, Province_FR) %>%
+  group_by(SpeciesID) %>%
+  arrange(SpeciesID, Province_FR) %>%
+  summarise(Range_FR = paste(Province_FR, collapse = ", "))
+
+            # Join
+Species %<>%
+  left_join(., range_en, by="SpeciesID") %>%
+  left_join(., range_fr, by="SpeciesID")
+
+      # Reference population size
+            # Get estimates that are relevant to the KBA Registry (i.e. most recent # of mature individuals global/national estimate)
+relevantReferenceEstimates <- SpeciesAssessment %>%
+  left_join(., SpeciesAtSite[,c("SpeciesAtSiteID", "SpeciesID")], by="SpeciesAtSiteID") %>%
+  filter(AssessmentParameter == "(i) number of mature individuals") %>% # Only retain the record if the assessment parameter = number of mature individuals
+  group_by(SpeciesID, KBALevel) %>%
+  arrange(SpeciesID, KBALevel, desc(DateAssessed)) %>%
+  filter(row_number()==1) # Only retain the most recent SpeciesAssessment per species and per KBALevel
+
+            # Add the estimates the Species table
+Species <- relevantReferenceEstimates %>%
+  select(SpeciesID, KBALevel, ReferenceEstimate_Best, ReferenceEstimate_Sources) %>%
+  rename(PopulationSize = ReferenceEstimate_Best,
+         Citation = ReferenceEstimate_Sources) %>%
+  pivot_wider(names_from = KBALevel, values_from = c(PopulationSize, Citation), names_glue = "{KBALevel}{.value}") %>%
+  rename(CitationGlobalPopulation = GlobalCitation,
+         CitationNationalPopulation = NationalCitation) %>%
+  left_join(Species, ., by="SpeciesID")
+
+      # Select final columns
+Species %<>%
+  select(all_of(crosswalk %>% filter(Layer_BC == "Species") %>% pull(Name_BC)))
+
+      # Save
+arc.write(paste0(outputDB, "/Species"), Species, overwrite = T)
+
+# Species_Citation
+      # Create
+Species_Citation <- PopSizeCitation %>%
+  filter(!Scope == "Site") %>%
+  filter(SpeciesAssessmentID %in% relevantReferenceEstimates$SpeciesAssessmentID) %>%
+  left_join(., KBACitation, by="KBACitationID") %>%
+  left_join(., SpeciesAssessment, by="SpeciesAssessmentID") %>%
+  left_join(., SpeciesAtSite, by="SpeciesAtSiteID") %>%
+  distinct(SpeciesID, KBACitationID, .keep_all = T) %>% # Only keep each citation once per species (this line is necessary for cases where the same citation was used for both a global and a national assessment for the species)
+  mutate(SpeciesCitationID = 1:nrow(.)) %>%
+  select(all_of(crosswalk %>% filter(Layer_BC == "Species_Citation") %>% pull(Name_BC)))
+
+      # Save
+arc.write(paste0(outputDB, "/Species_Citation"), Species_Citation, overwrite = T)
