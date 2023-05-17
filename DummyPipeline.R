22ddc8968c400361675d34435f107c6663e20c24
Chloé Debyser <chloe.debyser@gmail.com>
2023-05-17 15:12:09 -0400

    Create DummyPipeline.R

---
+++ DummyPipeline.R
@@ -0,0 +1,40 @@
+# Packages
+library(httr)
+library(tidyverse)
+library(geojsonsf)
+library(sf)
+library(magrittr)
+
+# Input parameters
+username <- "kbapipeline"
+password
+address <- "Restricted/FeatureServer/4"
+spatial <- F
+
+# Get token
+response <- httr::POST("https://gis.natureserve.ca/portal/sharing/rest/generateToken",
+                       body = list(username=username, password=password, referer=":6443/arcgis/admin",f="json"),
+                       encode = "form")
+token <- content(response)$token
+
+# Get GeoJSON
+url <- parse_url("https://gis.natureserve.ca/arcgis/rest/services")
+url$path <- paste(url$path, paste0("EBAR-KBA/", address, "/query"), sep = "/")
+url$query <- list(where = "OBJECTID >= 0",
+                  outFields = "*",
+                  returnGeometry = "true",
+                  f = "geojson")
+request <- build_url(url)
+response <- VERB(verb = "GET",
+                 url = request,
+                 add_headers(`Authorization` = paste("Bearer ", token)))
+data <- content(response, as="text") %>%
+  geojson_sf()
+
+# If non-spatial, drop geometry
+if(!spatial){
+  data %<>% st_drop_geometry()
+}
+
+# Get number of species
+NSpecies <- nrow(data)
