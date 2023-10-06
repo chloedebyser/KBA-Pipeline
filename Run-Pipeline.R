19775b75b120f5903559ea43c045ee69638462ed
Dean Evans <73301622+deanrobertevans@users.noreply.github.com>
2023-10-06 11:24:23 -0400

    Update Run-Pipeline.R

--- Run-Pipeline.R
+++ Run-Pipeline.R
@@ -3,7 +3,7 @@ rm(list = ls())
 # 2. Set working directory
 setwd("/home/rstudio/KBA-Pipeline")
 # 3. Git Pull most recent version
-system("git pull")
+system("sudo git pull")
 # 4. Run Pipeline
 source("KBA-Pipeline.R")
 # 5. Clean Environment
