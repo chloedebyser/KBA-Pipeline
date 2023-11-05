d89670831165994a3e5a8572733fc73fcb9c6753
Chloé Debyser <chloe.debyser@gmail.com>
2023-11-05 09:10:29 -0500

    Correct preparation of the email notification list

--- KBA-Pipeline.R
+++ KBA-Pipeline.R
@@ -1580,7 +1580,7 @@ if(nrow(siteNotifications) > 0){
     
     # Vector of emails to notify
     notificationEmails <-  c("devans@birdscanada.org", "abichel@birdscanada.org","acouturier@bsc-eoc.org", "cdebyser@wcs.org", "psoroye@wcs.org", "craudsepp@wcs.org", "aleung@wcs.org") %>%
-      c(., trimws(strsplit(siteNotificationsNew$leademail, ";")[[1]])) %>%
+      c(., trimws(unlist(strsplit(siteNotificationsNew$leademail, ";")))) %>%
       unique()
     
     # Send email if on production
