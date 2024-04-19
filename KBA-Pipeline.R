6fd9ceb96e3d4f360a28690c53fb3f3c2521565f
Chloé Debyser <chloe.debyser@gmail.com>
2024-04-19 12:53:59 -0400

    Do not reach COSEWIC range maps from KBA-EBAR database

--- KBA-Pipeline.R
+++ KBA-Pipeline.R
@@ -57,7 +57,7 @@ COSEWICLinks <- read.xlsx("COSEWIC_Links.xlsx")
 
 # KBA-EBAR database information
 Sys.sleep(20)
-read_KBAEBARDatabase(datasetNames=c("DatasetSource", "InputDataset", "ECCCRangeMap", "RangeMap", "EmptyRangeMap"),
+read_KBAEBARDatabase(datasetNames=c("DatasetSource", "InputDataset", "ECCCRangeMap", 'COSEWICRangeMap', "RangeMap", "EmptyRangeMap"),
                      type="exclude",
                      account="kbapipeline",
                      epsg=4326) %>%
